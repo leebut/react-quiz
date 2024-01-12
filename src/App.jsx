a82792b2960e3195c3f1aee51afc0c7dff9e1468
leebut <lee@leesenglish.com>
2024-01-12 13:44:23 +0800

    data dir created + json-server installed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
+import { useEffect } from "react";
 import "./App.css";
+import Header from "./Header.jsx";
+import Main from "./Main.jsx";
 
-export default function App() {}
+export default function App() {
+  useEffect(function () {
+    fetch("http://127.0.0.1:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error("Error"));
+  });
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question</p>
+      </Main>
+    </div>
+  );
+}
