aef0ccf41ae8129f93c5b511031b7a3f2de4bd59
Lee <lee@leesenglish.com>
2024-01-12 23:29:31 +0800

    Update: reducer and component additions.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,52 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import "./App.css";
 import Header from "./Header.jsx";
 import Main from "./Main.jsx";
+import Loader from "./Loader.jsx";
+import Error from "./Error.jsx";
+import StartScreen from "./StartScreen.jsx";
+import Question from "./Question.jsx";
+
+const initialState = {
+  questions: [],
+
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
+    default:
+      throw new Error("You cannot do that!");
+  }
+}
 
 export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+
   useEffect(function () {
     fetch("http://127.0.0.1:8000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error("Error"));
-  });
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
