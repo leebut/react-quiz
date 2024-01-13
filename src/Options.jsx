0e535ad689468801bd6cd1deb647875cdcd5b24b
Lee <lee@leesenglish.com>
2024-01-13 21:31:53 +0800

    Update: added conditional rendering to options.

--- src/Options.jsx
+++ src/Options.jsx
@@ -1,9 +1,11 @@
-function Options({ question, answer, dispatch }) {
+function Options({ question, dispatch, answer }) {
   return (
     <div className="options">
       {question.options.map((option, index) => (
         <button
-          className="btn btn-option"
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            index === question.correctOption ? "correct" : "wrong"
+          }`}
           key={option}
           onClick={() => dispatch({ type: "newAnswer", payload: index })}
         >
